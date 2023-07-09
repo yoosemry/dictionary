42efc4976c6db0a704f91b3e03712dd3893723b8
yoosemry <mcyoose@gmail.com>
2023-07-09 11:13:05 +0300

    updated 4

--- js.js
+++ js.js
@@ -1,36 +1,33 @@
 const search = document.getElementById("search");
 const selection = document.getElementById("selection");
-const dis = document.querySelector('.displays')
+const dis = document.querySelector(".displays");
 
-let findWords;
-dis.innerHTML = ''
+let dtInfo;
+dis.innerHTML = "";
 
-const fetchData = async (searchText)=>{
+const fetchData = async (searchText) => {
+  const response = await fetch("/word.json");
+  const data = await response.json();
+  findWords = data.find((word) => word.word == searchText);
+  return findWords;
+};
 
-    const response = await fetch("/word.json");
-    const data = await response.json();
-    findWords = data.find((word) => word.word == searchText);
-    return findWords;
-  
-}
+const getMemories =  () => {
+  let memo = localStorage.getItem("memories");
+  return memo ? JSON.parse(memo) : [];
+};
 
+const memories = (memo) => {
+  const getMem =  getMemories();
 
+  getMem.push(memo);
 
-const getMemories = ()=>{
-    return localStorage.getItem('memories') ?  JSON.parse(localStorage.getItem('memories')) : [];
-}
-const memories = async ()=>{
-   const getMem = getMemories();
-  const wordsFetch = await fetchData(search.value);
- 
-  console.log(getMem)
-
-  localStorage.setItem('memories', JSON.stringify(upd))
-      
-}
-const displayFun = async(string,parts)=>{
-    
-    const allinfo = `
+  localStorage.setItem("memories", JSON.stringify(getMem));
+  search.value = "";
+  dis.innerHTML = "";
+};
+const displayFun = async (string, parts) => {
+  const allinfo = `
     <div class="display">
 
    
@@ -47,31 +44,31 @@ const displayFun = async(string,parts)=>{
         <h4>examples</h4>
         <ul class="lists">
        
-       ${string.examples.map(exam => `<li>${exam}</li>`)}
+       ${string.examples.map((exam) => `<li>${exam}</li>`)}
         
    
         </ul>
    
         <h4>Synonyms</h4>
         <ul class="lists">
-        ${string.synonyms.map(exam => `<li>${exam}</li>`)}
+        ${string.synonyms.map((exam) => `<li>${exam}</li>`)}
    
         </ul>
 
         <h4>Antonyms</h4>
         <ul class="lists">
-        ${string.antonyms.map(exam => `<li>${exam}</li>`)}
+        ${string.antonyms.map((exam) => `<li>${exam}</li>`)}
    
         </ul>
 
         <div class="learned">
-            <input type="checkbox" name="learned" id="learned" onclick="memories()"> Learning
+            <input type="checkbox" name="learned" id="learned" onclick="memories(dtInfo)"> Learning
         </div>
     </div>
 </div>
 
-`
-const example = `
+`;
+  const example = `
 <div class="display">
 
    
@@ -81,7 +78,7 @@ const example = `
     <h4>examples</h4>
     <ul class="lists">
    
-   ${string.examples.map(exam => `<li>${exam}</li>`)}
+   ${string.examples.map((exam) => `<li>${exam}</li>`)}
     
 
     </ul>
@@ -90,9 +87,9 @@ const example = `
 </div>
 </div>
 
-`
+`;
 
-const synonyms = `
+  const synonyms = `
 <div class="display">
 
    
@@ -102,13 +99,13 @@ const synonyms = `
 
     <h4>Synonyms</h4>
     <ul class="lists">
-    ${string.synonyms.map(exam => `<li>${exam}</li>`)}
+    ${string.synonyms.map((exam) => `<li>${exam}</li>`)}
 
     </ul>
 
-`
+`;
 
-const antonyms = `
+  const antonyms = `
 <div class="display">
 
    
@@ -120,13 +117,13 @@ const antonyms = `
 
     <h4>Antonyms</h4>
     <ul class="lists">
-    ${string.antonyms.map(exam => `<li>${exam}</li>`)}
+    ${string.antonyms.map((exam) => `<li>${exam}</li>`)}
 
     </ul>
 
-`
+`;
 
-const defin = `
+  const defin = `
 
 <div class="display">
 
@@ -143,92 +140,76 @@ const defin = `
   
  
 
-`
+`;
 
-    if(parts == 'all'){
-     dis.insertAdjacentHTML('afterbegin', allinfo)
-       
-    }else if (parts == 'definition'){
-        dis.insertAdjacentHTML('afterbegin', defin)
-   
-    }else if(parts == 'example'){
-        dis.insertAdjacentHTML('afterbegin', example)
-   
-    }else if(parts == 'synonyms'){
-        dis.insertAdjacentHTML('afterbegin', synonyms)
-       
-    }else if(parts == 'antonyms'){
-        dis.insertAdjacentHTML('afterbegin', antonyms)
-   
-    }else{
-       return
-    }
-   
-   
-   }
-   
-
-
+  if (parts == "all") {
+    dis.insertAdjacentHTML("afterbegin", allinfo);
+  } else if (parts == "definition") {
+    dis.insertAdjacentHTML("afterbegin", defin);
+  } else if (parts == "example") {
+    dis.insertAdjacentHTML("afterbegin", example);
+  } else if (parts == "synonyms") {
+    dis.insertAdjacentHTML("afterbegin", synonyms);
+  } else if (parts == "antonyms") {
+    dis.insertAdjacentHTML("afterbegin", antonyms);
+  } else {
+    return;
+  }
+};
 
 selection.addEventListener("change", async (e) => {
   const { value } = e.target;
 
-  if(!search.value){
-    alert('search is empty')
-    return
+  if (!search.value) {
+    alert("search is empty");
+    return;
   }
   try {
     const response = await fetch("/word.json");
     const data = await response.json();
     let findWords = data.find((word) => word.word == search.value);
-    if(!findWords) {
-        alert(`"${search.value}" ma ahan Mid no diban gashan`)
-    
-    return
-};
-    if (!response.ok) throw new Error("link wax ka qaldan");
+    if (!findWords) {
+      alert(`"${search.value}" ma ahan Mid no diban gashan`);
 
+      return;
+    }
+
+    dtInfo = findWords;
+    if (!response.ok) throw new Error("link wax ka qaldan");
 
     switch (value) {
       case "all":
-        dis.innerHTML = ''
-        displayFun(findWords,value);
-        
-        
+        dis.innerHTML = "";
+        displayFun(findWords, value);
+
         break;
 
       case "definition":
-        dis.innerHTML = ''
-        displayFun(findWords,value)
-
+        dis.innerHTML = "";
+        displayFun(findWords, value);
 
         break;
 
       case "example":
-        dis.innerHTML = ''
-      displayFun(findWords,value)
+        dis.innerHTML = "";
+        displayFun(findWords, value);
         break;
 
       case "synonyms":
-        dis.innerHTML = ''
-      displayFun(findWords,value)
+        dis.innerHTML = "";
+        displayFun(findWords, value);
         break;
 
       case "antonyms":
-        dis.innerHTML = ''
-      displayFun(findWords,value)
+        dis.innerHTML = "";
+        displayFun(findWords, value);
         break;
 
       default:
-        alert('empty')
+        alert("empty");
         break;
     }
-
-  
   } catch (error) {
     alert(error);
   }
 });
-
-
-
